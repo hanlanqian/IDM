24c998891c2123270106121f1291ec0eab1d7ddd
hanlanqian <1652433642@qq.com>
2021-05-22 15:38:25 +0800

    1

--- Pyqt_IDM/request.py
+++ Pyqt_IDM/request.py
@@ -11,8 +11,8 @@ video_urls = you_get.main()
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
     'Referer': 'https://space.bilibili.com/4899781/',
-    'Origin': 'http://space.bilibili.com',
-    'Host': 'space.bilibili.com',
+    'Origin': 'http://www.bilibili.com',
+    # 'Host': 'space.bilibili.com',
     'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
 }
